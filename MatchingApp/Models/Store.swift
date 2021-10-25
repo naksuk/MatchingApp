67ce4d765445ece450a025ad35cfe34f2eb3ac96
“naksuk <n4ksuk@gmail.com>
2021-10-25 22:56:55 +0900

    簡易店舗モデル作成

--- MatchingApp/Models/Store.swift
+++ MatchingApp/Models/Store.swift
@@ -3,11 +3,11 @@ import UIKit
 
 class Store {
     
-    //var name: String
+    var name: String
     var image: UIImage
     
     init(dic: [String: Any]) {
-        //self.name = dic["name"] as? String ?? ""
+        self.name = dic["name"] as? String ?? ""
         self.image = dic["image"] as! UIImage
     }
     
